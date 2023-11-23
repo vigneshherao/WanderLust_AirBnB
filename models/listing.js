72ee26418f50433321fde0d4d2f7e84069185406
vignesh <vigneshfornavy@gmail.com>
2023-11-23 21:02:31 +0530

    listing model added

---
+++ models/listing.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title:{
+        type:String,
+        required:true
+
+    },
+    description:String,
+    image:String,
+    price:Number,
+    location:String,
+    country:String
+})
+
+
+const Listing = mongoose.model("Listing",listingSchema);
+
+
+module.exports = Listing;
